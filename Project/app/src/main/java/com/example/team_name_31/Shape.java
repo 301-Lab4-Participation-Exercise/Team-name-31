c68fa09d2b511027ca8dd777c123e7e6d2cc7216
LogarthmicJones <loganjones9117@gmail.com>
2022-02-02 17:28:54 -0700

    Removed uneccessary comments + resolved conflict

--- Project/app/src/main/java/com/example/team_name_31/Shape.java
+++ Project/app/src/main/java/com/example/team_name_31/Shape.java
@@ -3,9 +3,5 @@ package com.example.team_name_31;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
-=======
     String color = "Orange"
-
->>>>>>> c0aa1b952a5538c5d02cf45b2e70f76af601bd51
 }
